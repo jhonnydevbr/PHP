7eff47b6019e37268f72c2eb1f5984f68fc5df6b
jhonnydevbr <jhonnybrustolin@gmail.com>
2020-07-28 15:23:12 -0300

    Efetuado alteração nos arquivos

--- variaveis-em-php.php
+++ variaveis-em-php.php
@@ -8,9 +8,11 @@
 	$nome = "Jhonny";
 
 	//Mostrando o valor da variavel na tela
+
 	echo $nome;
 
 	//Para saber informações sobre a variavél... o comando var_dump é muito útil quando você utiliza array, para ver o os valores dentro da variavel;
+
 	var_dump($nome);
 
 	//Sempre que for criar a variavel, importante sempre criar um nome que você saiba o que significa, de preferência, CamelCase(Cada inicio da palavra com letra maiuscula). 
@@ -20,5 +22,24 @@
 
 	$anoNascimento = 1998;
 	$nomeCompleto = "Jhonny Brustolin";
+
+	//Importante: Cuidado com os nomes reservados no PHP; For Example: $this, nao tem como, pois this é uma palavra reservada da linguagem;
+
+	//Comando para limpar as variavéis, pode ser uma ou várias ao mesmo tempo: unset($nome, $anoNascimento, $nomeCompleto);
+
+	//Condição para mostrar a variavél somente se ela existir
+
+	if (isset($nome)) {
+
+		echo $nome;
+
+	}
+
+	//Concatenação:
+
+	$cliente = $nomeCompleto . " " . $anoNascimento;
+
+	echo $cliente;
+
 	
 ?>

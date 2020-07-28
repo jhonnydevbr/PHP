d2c0ebbfeb6875e0bbea24bd9807eb54c0fefed1
jhonnydevbr <jhonnybrustolin@gmail.com>
2020-07-28 14:56:01 -0300

    Colocado alguns pontos sobre variaveis em PHP

--- variaveis-em-php.php
+++ variaveis-em-php.php
@@ -1,9 +1,17 @@
 <?php
 	
-	echo "Utilizando variavéis no PHP";
+	//echo "Utilizando variavéis no PHP";
 
+	//echo "A variavél é uma parte alocada na memória do servidor";
+	
+	//Declaração e atribuição de um valor em uma variavél
 	$nome = "Jhonny";
 
-	
+	//Mostrando o valor da variavel na tela
+	echo $nome;
+
+	//Para saber informações sobre a variavél... o comando var_dump é muito útil quando você utiliza array, para ver o os valores dentro da variavel;
+	var_dump($nome);
+
 	
 ?>

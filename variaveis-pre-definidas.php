bd67fbf986e9cd4a3d18b614db8bc96238a11a15
jhonnydevbr <jhonnybrustolin@gmail.com>
2020-07-28 17:21:36 -0300

    Efetuado alteração no arquivo variaveis-pre-definidas

--- variaveis-pre-definidas.php
+++ variaveis-pre-definidas.php
@@ -1,5 +1,15 @@
-<?echo
+<?php
 	
-	echo "Variavéis Pré-Definidas";
+	echo "Variavéis Pré-Definidas"; echo "<br>";
+
+	//Também conhecidas como arrays super globais(podem ser acessadas em qualquqer escopo em qualquer ponto)
+	//São variavéis internas do PHP que já vem com alguns recursos, tanto externa(GET E POST que pegam informações externas) quanto ambiente.
+
+	$nome = $_GET["a"];
+
+	//var_dump($nome);
 	
+	$ip = $_SERVER["REMOTE_ADDR"];
+
+	echo $ip;
 ?>

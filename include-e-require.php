a97a9920961a4379e367622fb866d4096de82286
jhonnydevbr <jhonnybrustolin@gmail.com>
2020-07-30 00:41:35 -0300

    Criado arquivo para descrever brevemente include e require no PHP

---
+++ include-e-require.php
@@ -0,0 +1,13 @@
+<?php
+	echo "Include e Require em PHP"; echo "<br>";
+
+	//Você consegue fazer que um código em PHP possa ser usado/invocado em vários arquivos.
+		// Include: você chama um trecho de código, excemplo: Criar um arquivo com todas as funções que você irá precisar, não precisa ficar escrevendo as funções em toda página, somente chama ela do arquivo usando include.
+
+		// Require: Requise obriga que o arquivo exista e que esteja funcionando perfeitamente.
+			//Require é mais interessante para segurança;
+
+		// Require once / Include once, o PHP garante que o arquivo seja chamado somente 1 vez.
+
+
+?>

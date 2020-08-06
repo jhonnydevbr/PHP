168642ce08d43f8da389206ea4b0b98a6ac4eef5
jhonnydevbr <jhonnybrustolin@gmail.com>
2020-08-06 15:26:03 -0300

    Efetuado alteração nos arquivos

--- estruturas-de-controle-e-lacos-de-repeticao/estruturas-de-controle-e-lacos-de-repeticao.php
+++ estruturas-de-controle-e-lacos-de-repeticao/estruturas-de-controle-e-lacos-de-repeticao.php
@@ -92,6 +92,28 @@
 
 		echo "FOR";	echo "<br>";
 
+		for ($i = 0; $i < 10; $i++){
+
+			echo $i . "<br>";
+
+		}
+
+		for ($i = 0; $i < 1000; $i+=5){
+
+			echo $i . "<br>";
+
+		}
+
+		for ($i = 0; $i < 1000; $i+=5){
+
+			if($i > 200 && $i < 800) continue;
+
+			if($i === 900) break;
+
+			echo $i . "<br>";
+
+		}
+
 
 
 
